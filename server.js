a82a5fefddedf474b568b20c2d2a7bc6facbf750
BOUAB Ahmed <bouab.ahmed757@gmail.com>
2023-01-20 22:04:46 +0100

    edit mongoose setup : adding mongo atlas

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 require('dotenv').config();
 const multer = require('multer');
-const mongoose = require('mongoose');
+const connectDB = require('./config/db');
 const express = require('express');
-
+const bcrypt = require('bcrypt');
+const File = require('./models/File');
 const app = express();
 
 const upload = multer({ dest: 'uploads' });
 
-mongoose.connect(process.env.MONGO_URL);
+connectDB();
 
 app.set('view engine', 'ejs');
 
@@ -15,8 +16,20 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.post('/upload', upload.single('file'), (req, res) => {
-  res.send('ok');
+app.post('/upload', upload.single('file'), async (req, res) => {
+  const fileData = {
+    path: req.file.path,
+    originalName: req.file.originalname,
+  };
+
+  if ((req.body.password != null && req.body.password) !== '') {
+    fileData.password = await bcrypt.hash(req.body.password, 10);
+  }
+
+  const file = new File(fileData);
+  await file.save();
+  console.log(file);
+  res.send(fileData.originalName);
 });
 
 app.listen(process.env.PORT);
