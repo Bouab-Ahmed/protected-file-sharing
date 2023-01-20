cd6d18d0da128a5d122bb6e815735f9da67682ee
BOUAB Ahmed <bouab.ahmed757@gmail.com>
2023-01-20 23:21:19 +0100

    add password protection

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 const multer = require('multer');
-const connectDB = require('./config/db');
-const express = require('express');
 const bcrypt = require('bcrypt');
 const File = require('./models/File');
+const connectDB = require('./config/db');
+
+const express = require('express');
 const app = express();
+app.use(express.urlencoded({ extended: true }));
 
 const upload = multer({ dest: 'uploads' });
 
@@ -28,8 +30,33 @@ app.post('/upload', upload.single('file'), async (req, res) => {
 
   const file = new File(fileData);
   await file.save();
-  console.log(file);
-  res.send(fileData.originalName);
+  res.render('index', { filelink: `${req.headers.origin}/file/${file.id}` });
 });
+
+app.route('/file/:id').get(handlePassword).post(handlePassword);
+
+async function handlePassword(req, res) {
+  const file = await File.findById(req.params.id);
+
+  if (file.password != null) {
+    if (req.body.password == null) {
+      res.render('password');
+      return;
+    }
+  }
+
+  if (
+    file.password != null &&
+    !(await bcrypt.compare(req.body.password, file.password))
+  ) {
+    res.render('password', { error: 'Incorrect password' });
+    return;
+  }
+
+  file.downloadCount++;
+  await file.save();
+
+  res.download(file.path, file.originalName);
+}
 
 app.listen(process.env.PORT);
