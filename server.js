9b7450954e44bd7b5aef3c48da4d4dcf7d83784a
BOUAB Ahmed <bouab.ahmed757@gmail.com>
2023-01-20 15:01:28 +0100

    multer setup

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const multer = require('multer');
 
 const app = express();
+
+const upload = multer({ dest: 'uploads' });
 
 app.set('view engine', 'ejs');
 
@@ -8,7 +11,7 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.post('/upload', (req, res) => {
+app.post('/upload', upload.single('file'), (req, res) => {
   res.send('ok');
 });
 
